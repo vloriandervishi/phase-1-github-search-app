7808e5f9ef4fdb322c2592ed2fb46192927837d8
vlorian <vlorian.dervishi@gmail.com>
2023-08-24 23:50:04 -0400

    added fetch with github api

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
-const uri = "https://developer.github.com/v3/";
+function gitUsers(user) {
+  fetch(`https://api.github.com/search/users?q=${user}`)
+    .then((response) => response.json())
+    .then((show) => {
+      Object.values(show.items).forEach((users) => {
+        const avatar = users.avatar_url;
+        const login = users.login;
+        const url = users.html_url;
+        const userListEl = document.querySelector("#user-list");
+        const loginEl = document.createElement("li");
+        const avatarEl = document.createElement("img");
+        const urlEl = document.createElement("li");
+        loginEl.textContent = login;
+        avatarEl.src = avatar;
+        urlEl.textContent = url;
+        userListEl.append(loginEl, avatarEl, urlEl);
+        // username, avatar and a link to their profile.
+      });
+    })
+    .catch((error) => console.log(error.message));
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  function gitUsers() {
-    fetch(uri)
-      .then((response) => response.json())
-      .then((show) => console.log(user));
-  }
-  gitUsers();
+  const getUserEl = document.getElementById("github-form");
+
+  getUserEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(getUserEl.search.value);
+    const findUser = getUserEl.search.value;
+    gitUsers(findUser);
+  });
 });
